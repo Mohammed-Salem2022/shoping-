95faf94860455fff10a3d53e2cb5415914dc6ca0
Mohammed <devrmohammed1@gmail.com>
2022-06-13 17:41:39 +0300

    desgin

--- lib/view/widgets/product_Delails/image_Slider.dart
+++ lib/view/widgets/product_Delails/image_Slider.dart
@@ -3,11 +3,17 @@
 
 
 
+
+
+import 'package:badges/badges.dart';
 import 'package:carousel_slider/carousel_slider.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:get/get_core/src/get_main.dart';
+import 'package:shoping_f/controller/card_controller.dart';
+import 'package:shoping_f/routes/routes.dart';
 import 'package:shoping_f/utils/theme.dart';
 import 'package:smooth_page_indicator/smooth_page_indicator.dart';
 
@@ -52,11 +58,11 @@ class ImageSliderState extends State<ImageSlider> {
                enlargeCenterPage: true,
             enableInfiniteScroll: false,
             viewportFraction: 2,
-            autoPlayInterval: Duration(seconds: 2),
+            autoPlayInterval: Duration(seconds:3),
             onPageChanged: (index,reason){
 
                setState(() {
-                 currentpage= index;
+                currentpage= index;
                });
 
             }
@@ -92,16 +98,19 @@ class ImageSliderState extends State<ImageSlider> {
 
             ,),
          Positioned(
+
+
+           //هنا تصميم اختيار الالوان
              bottom: MediaQuery.of(context).size.height*0.03,
              left: MediaQuery.of(context).size.width*0.80,
              child: Container(
                height: MediaQuery.of(context).size.height*0.25,
                width: MediaQuery.of(context).size.width*0.07,
-           decoration: BoxDecoration(
+             decoration: BoxDecoration(
              color: Colors.black.withOpacity(0.4),
              borderRadius: BorderRadius.circular(15)
            ),
-            child: ListView.separated(
+              child: ListView.separated(
                 itemBuilder:  (context,index){
                   return GestureDetector(
                     onTap: (){
@@ -117,8 +126,82 @@ class ImageSliderState extends State<ImageSlider> {
                 },
                 separatorBuilder: (context,index)=> SizedBox(height: 5),
                 itemCount: Colorselected.length)
-         ))
+         )),
+      desginHeadPage(),
     ],);
   }
 }
 
+
+
+
+
+
+Widget desginHeadPage()  {
+final contrller= Get.find<CardController>();
+
+// here desgin Head Page
+  return Row(
+    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+    children: [
+     InkWell(
+       onTap: (){
+           Get.back();
+
+       },
+       child:     Container(
+           margin:EdgeInsets.all(20) ,
+
+           decoration: BoxDecoration(
+             color: Get.isDarkMode?pinkClr:mainColor,
+
+             shape: BoxShape.circle,
+           ),
+           child:  const Icon(Icons.arrow_back,
+             size: 30,
+             color:Colors.white,)
+       )
+     ),
+      InkWell(onTap: (){
+
+          Get.offNamed(Namepages.CardScreen);
+      },
+         child:FutureBuilder<QuerySnapshot>(
+             future:contrller.getAllDataFromFirebase() ,
+             builder: (context,snapshot){
+               return       Badge(
+                 position: BadgePosition.topEnd(top: 10, end: 13),
+                 animationDuration: Duration(milliseconds: 300),
+                 animationType: BadgeAnimationType.slide,
+                 badgeContent:  Text(
+                   '${snapshot.data?.docs.length}',
+                   style: TextStyle(color: Colors.white),
+                 ),
+                 child: Container(
+                     margin:EdgeInsets.all(20) ,
+                     padding:EdgeInsets.all(8)  ,
+
+                     decoration: BoxDecoration(
+                       color: Get.isDarkMode?pinkClr:mainColor,
+
+                       shape: BoxShape.circle,
+                     ),
+                     child:  const Icon(Icons.shopping_cart,
+                       size: 20,
+                       color:Colors.white,)
+                 ),
+               );
+             }),
+
+
+
+
+
+
+
+
+
+      ),
+    ],);
+
+}
